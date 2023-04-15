559ef248f90ce4ac6b68083f3aa8027fb2900e1a
brianmchugh <brianmchugh02@gmail.com>
2023-04-14 22:25:11 -0400

    Added criteria information

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ table = soup.find('table', {'class': 'wikitable sortable'})
 rows = table.find_all('tr')[1:]
 sp500_tickers = [row.find_all('td')[0].text.strip() for row in rows]
 
+# Define screening criteria
+pe_ratio_threshold = 15
+pb_ratio_threshold = 1.5
+dividend_yield_threshold = 0.02
+debt_to_equity_threshold = 0.5
+earnings_growth_threshold = 0.05
+financial_modeling_prep_api_key = 'your_api_key_here'
+
+# Initialize a DataFrame to store the results
+screened_stocks = pd.DataFrame(columns=['Ticker', 'P/E Ratio', 'P/B Ratio', 'Dividend Yield', 'Debt to Equity', '5-Year Earnings Growth'])
+
+
