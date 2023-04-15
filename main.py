3c4e1dabc47f09dcf4dc63d38b99b8a1dd4949c4
brianmchugh <brianmchugh02@gmail.com>
2023-04-15 10:46:34 -0400

    Added a list for all stocks that meet the basic criteria

--- main.py
+++ main.py
@@ -24,4 +24,27 @@ financial_modeling_prep_api_key = 'your_api_key_here'
 # Initialize a DataFrame to store the results
 screened_stocks = pd.DataFrame(columns=['Ticker', 'P/E Ratio', 'P/B Ratio', 'Dividend Yield', 'Debt to Equity', '5-Year Earnings Growth'])
 
+# Initialize an empty list to store the tickers that meet the criteria
+selected_tickers = []
 
+for ticker in sp500_tickers:
+        try:
+            stock = yf.Ticker(ticker)
+
+            # Fetch key statistics
+            info = stock.info
+            pe_ratio = info['trailingPE']
+            pb_ratio = info['priceToBook']
+            dividend_yield = info['dividendYield']
+
+            if (pe_ratio < pe_ratio_threshold and
+                pb_ratio < pb_ratio_threshold and
+                dividend_yield > dividend_yield_threshold):
+
+                # Add the ticker to the selected_tickers list
+                selected_tickers.append(ticker)
+
+        except Exception as e:
+            print(f"Error fetching data for {ticker}: {e}")
+
+print(selected_tickers)
