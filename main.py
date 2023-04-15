b24223624fb4debf2054667d464c3e82dc5da406
brianmchugh <brianmchugh02@gmail.com>
2023-04-14 21:46:23 -0400

    Try using beautifulsoup to get S&P 500 tickers

--- main.py
+++ main.py
@@ -3,7 +3,14 @@
 import pandas as pd
 import yfinance as yf
 import requests
+import bs4 as BeautifulSoup
 
 # Fetch S&P 500 components
-sp500_table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
-sp500_tickers = sp500_table['Symbol'].tolist()
+url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'lxml')
+table = soup.find('table', {'class': 'wikitable sortable'})
+rows = table.find_all('tr')[1:]
+sp500_tickers = [row.find_all('td')[0].text.strip() for row in rows]
+
+print(sp500_tickers)
